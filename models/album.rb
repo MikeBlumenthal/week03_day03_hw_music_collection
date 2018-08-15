b13de3ff1983ca628758743267049a6daba151a1
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-15 14:07:32 +0100

    adds initialize methods for the classes and an sql runner

--- models/album.rb
+++ models/album.rb
@@ -1,1 +1,14 @@
 require ('pg')
+
+class Album
+
+  attr_reader :id, :title, :genre, :artist_id
+
+  def initialize(info)
+    @id = info['id'].to_i if info['id']
+    @title = info['title']
+    @genre = info['genre']
+    @artist_id = info['artist_id'].to_i
+  end
+
+end
--- models/artist.rb
+++ models/artist.rb
@@ -1,1 +1,16 @@
 require ('pg')
+
+class Artist
+
+  attr_reader :id, :name
+
+  def initialize(info)
+    @id = info['id'].to_i if info['id']
+    @name = info['name']
+  end
+
+
+
+
+
+end
